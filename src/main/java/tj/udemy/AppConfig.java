20ffae09a76fea1793bc1e59b11b48234e4fe197
Tomasz Jarych <tjarych@gmail.com>
2019-01-12 17:00:59 +0100

    AOP configuration

--- src/main/java/tj/udemy/AppConfig.java
+++ src/main/java/tj/udemy/AppConfig.java
@@ -1,10 +1,9 @@
 package tj.udemy;
 
-import javax.persistence.EntityManagerFactory;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -14,10 +13,13 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import javax.persistence.EntityManagerFactory;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "tj.udemy")
 @EnableTransactionManagement
+@EnableAspectJAutoProxy
 public class AppConfig extends WebMvcConfigurerAdapter {
 
 	@Bean
